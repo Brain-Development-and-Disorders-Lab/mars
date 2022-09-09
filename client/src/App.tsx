c50652dbc518a7eed6c8ae4f132460debacf362b
Henry Burgess <henryjburg@gmail.com>
2022-09-09 16:01:46 -0500

    Update base App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,16 @@
 // React and Grommet
-import React, { useRef, useState } from "react";
-import {
-  Avatar,
-  Box,
-  Button,
-  Collapsible,
-  Drop,
-  Grommet,
-  Heading,
-} from "grommet/components";
-import { ResponsiveContext } from "grommet/contexts/ResponsiveContext";
-import {
-  AddCircle,
-  FormClose,
-  Layer,
-  Search as SearchIcon,
-  SettingsOption,
-  User,
-  View,
-} from "grommet-icons";
+import React from "react";
+import { Grommet } from "grommet/components";
 
 // Styling
 import "./styles/styles.css";
 import "@fontsource/roboto";
 
 // Routing
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Custom components
 import Navigation from "./view/components/Navigation";
-import Sidebar from "./view/components/Sidebar";
 
 // Custom pages
 import Start from "./view/pages/Create/Sample/Start";
@@ -45,211 +26,29 @@ import Samples from "./view/pages/View/Samples";
 import { theme } from "./theme";
 
 export const App = () => {
-  const [showSidebar, setShowSidebar] = useState(false);
-
-  const createRef = useRef(null);
-  const [createVisible, setCreateVisible] = useState(false);
-
-  const viewRef = useRef(null);
-  const [viewVisible, setViewVisible] = useState(false);
 
   return (
     <BrowserRouter>
       <Grommet theme={theme} full>
-        <ResponsiveContext.Consumer>
-          {(size) => (
-            <Box fill>
-              <Navigation>
-                <Heading level="3" margin="none">
-                  <Link to="/">🧪 Lab</Link>
-                </Heading>
-                <Box
-                  flex
-                  direction="row"
-                  gap="xlarge"
-                  pad={{ left: "medium", right: "small", vertical: "small" }}
-                >
-                  <Box direction="row" gap="small">
-                    <SearchIcon />
-                    <Heading level="4" margin="none">
-                      <Link to="/search">Search</Link>
-                    </Heading>
-                  </Box>
-                  <Box direction="row" gap="small">
-                    <Button
-                      ref={createRef}
-                      onClick={() => setCreateVisible(!createVisible)}
-                      plain
-                    >
-                      <Box direction="row" gap="small">
-                        <AddCircle />
-                        <Heading level="4" margin="none">
-                          Create
-                        </Heading>
-                      </Box>
-                    </Button>
-                    {createRef.current && createVisible && (
-                      <Drop
-                        align={{ top: "bottom", left: "left" }}
-                        target={createRef.current}
-                        elevation="large"
-                        margin={{ top: "medium" }}
-                      >
-                        <Box
-                          background="brand"
-                          pad="small"
-                          direction="column"
-                          gap="small"
-                        >
-                          <Heading level="4" margin="small">
-                            <Link
-                              to="#"
-                              onClick={() => setCreateVisible(false)}
-                            >
-                              Collection
-                            </Link>
-                          </Heading>
-                          <Heading level="4" margin="small">
-                            <Link
-                              to="/create/sample/start"
-                              onClick={() => setCreateVisible(false)}
-                            >
-                              Sample
-                            </Link>
-                          </Heading>
-                          <Heading level="4" margin="small">
-                            <Link
-                              to="#"
-                              onClick={() => setCreateVisible(false)}
-                            >
-                              Attributes
-                            </Link>
-                          </Heading>
-                        </Box>
-                      </Drop>
-                    )}
-                  </Box>
-                  <Box direction="row" gap="small">
-                    <Button
-                      ref={viewRef}
-                      onClick={() => setViewVisible(!viewVisible)}
-                      plain
-                    >
-                      <Box direction="row" gap="small">
-                        <View />
-                        <Heading level="4" margin="none">
-                          View
-                        </Heading>
-                      </Box>
-                    </Button>
-                    {viewRef.current && viewVisible && (
-                      <Drop
-                        align={{ top: "bottom", left: "left" }}
-                        target={viewRef.current}
-                        elevation="large"
-                        margin={{ top: "medium" }}
-                      >
-                        <Box
-                          background="brand"
-                          pad="small"
-                          direction="column"
-                          gap="small"
-                        >
-                          <Heading level="4" margin="small">
-                            <Link
-                              to="/collections"
-                              onClick={() => setViewVisible(false)}
-                            >
-                              Collections
-                            </Link>
-                          </Heading>
-                          <Heading level="4" margin="small">
-                            <Link
-                              to="/samples"
-                              onClick={() => setViewVisible(false)}
-                            >
-                              Samples
-                            </Link>
-                          </Heading>
-                        </Box>
-                      </Drop>
-                    )}
-                  </Box>
-                </Box>
-                <Button
-                  icon={<SettingsOption />}
-                  onClick={() => setShowSidebar(!showSidebar)}
-                />
-                <Avatar background="white">
-                  <User color="black" />
-                </Avatar>
-              </Navigation>
-              <Box
-                direction="row"
-                flex
-                overflow={{ horizontal: "hidden" }}
-                pad="small"
-              >
-                <Box flex align="left">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/create/sample/start" element={<Start />} />
-                    <Route
-                      path="/create/sample/associations"
-                      element={<Associations />}
-                    />
-                    <Route
-                      path="/create/sample/attributes"
-                      element={<Attributes />}
-                    />
-                    <Route path="/collections" element={<Collections />} />
-                    <Route path="/samples" element={<Samples />} />
-                    <Route path="/search" element={<Search />} />
-                    <Route path="samples">
-                      <Route path=":id" element={<Sample />} />
-                    </Route>
-                  </Routes>
-                </Box>
-                {!showSidebar || size !== "small" ? (
-                  <Collapsible direction="horizontal" open={showSidebar}>
-                    <Box
-                      flex
-                      width="medium"
-                      background="light-2"
-                      elevation="small"
-                      align="center"
-                    >
-                      <Sidebar />
-                    </Box>
-                  </Collapsible>
-                ) : (
-                  <Layer>
-                    <Box
-                      background="light-2"
-                      tag="header"
-                      justify="end"
-                      align="center"
-                      direction="row"
-                    >
-                      <Button
-                        icon={<FormClose />}
-                        onClick={() => setShowSidebar(false)}
-                      />
-                    </Box>
-                    <Box
-                      fill
-                      background="light-2"
-                      align="center"
-                      justify="center"
-                    >
-                      <Sidebar />
-                    </Box>
-                  </Layer>
-                )}
-              </Box>
-            </Box>
-          )}
-        </ResponsiveContext.Consumer>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create/sample/start" element={<Start />} />
+          <Route
+            path="/create/sample/associations"
+            element={<Associations />}
+          />
+          <Route
+            path="/create/sample/attributes"
+            element={<Attributes />}
+          />
+          <Route path="/collections" element={<Collections />} />
+          <Route path="/samples" element={<Samples />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="samples">
+            <Route path=":id" element={<Sample />} />
+          </Route>
+        </Routes>
       </Grommet>
     </BrowserRouter>
   );
--- client/src/view/components/Navigation.tsx
+++ client/src/view/components/Navigation.tsx
@@ -1,19 +1,81 @@
 // React and Grommet
-import React from "react";
-import { Box } from "grommet/components";
+import React, { useState } from "react";
+import {
+  Anchor,
+  Avatar,
+  Box,
+  Button,
+  Header,
+  Menu,
+  Nav,
+} from "grommet/components";
+import {
+  AddCircle,
+  Search as SearchIcon,
+  SettingsOption,
+  User,
+  View,
+} from "grommet-icons";
 
-const Navigation = (props: any) => (
-  <Box
-    tag="header"
-    direction="row"
-    align="center"
-    justify="between"
-    background="brand"
-    pad={{ left: "medium", right: "small", vertical: "small" }}
-    elevation="medium"
-    style={{ zIndex: "1" }}
-    {...props}
-  />
-);
+const Navigation = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  return (
+    <Header
+      background="brand"
+      sticky="scrollup"
+      pad="small"
+    >
+      <Nav align="center" direction="row">
+        <Anchor
+          label="Home"
+          href="/"
+          color="text"
+          size="large"
+        />
+        <Anchor
+          label="Search"
+          href="/search"
+          icon={<SearchIcon />}
+          color="text"
+          size="large"
+          reverse
+        />
+        <Menu
+          dropProps={{ align: { top: "bottom", left: "left" } }}
+          label="Create"
+          icon={<AddCircle />}
+          items={[
+            { label: "Collection" },
+            { label: <Anchor label="Sample" href="/create/sample/start" /> },
+            { label: "Attribute" },
+          ]}
+          size="large"
+        />
+        <Menu
+          dropProps={{ align: { top: "bottom", left: "left" } }}
+          label="View"
+          icon={<View />}
+          items={[
+            { label: <Anchor label="Collections" href="/collections" /> },
+            { label: <Anchor label="Samples" href="/samples" /> },
+            { label: <Anchor label="Attributes" href="/attributes" /> },
+          ]}
+          size="large"
+        />
+      </Nav>
+
+      <Box direction="row" align="center">
+        <Button
+          icon={<SettingsOption />}
+          onClick={() => setShowSidebar(!showSidebar)}
+        />
+        <Avatar background="white">
+          <User color="black" />
+        </Avatar>
+      </Box>
+    </Header>
+  );
+}
 
 export default Navigation;
