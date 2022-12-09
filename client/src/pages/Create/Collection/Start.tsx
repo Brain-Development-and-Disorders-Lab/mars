e91daca397d51662a7063d0466fa3196f386b329
Henry Burgess <henryjburg@gmail.com>
2022-12-09 14:13:38 -0600

    Update Collection creation page

--- client/src/pages/Create/Collection/Start.tsx
+++ client/src/pages/Create/Collection/Start.tsx
@@ -1,8 +1,8 @@
 // React and Grommet
 import React, { useState } from "react";
 import { SingleDatepicker } from "chakra-dayzed-datepicker";
-import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, Textarea } from "@chakra-ui/react";
-import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, Text, Textarea } from "@chakra-ui/react";
+import { CheckIcon, CloseIcon, InfoOutlineIcon } from "@chakra-ui/icons";
 
 // Navigation
 import { useLocation, useNavigate } from "react-router-dom";
@@ -46,93 +46,107 @@ export const Start = ({}) => {
 
   return (
     <Box m={"2"}>
-      <Flex direction={"column"} p={"2"} pt={"8"} pb={"8"} >
+      <Flex direction={"column"} p={"2"} pt={"8"} pb={"8"}>
         <Flex direction={"row"}>
           <Heading size={"2xl"}>Create Collection</Heading>
         </Flex>
       </Flex>
 
       <Flex p={"2"} pb={"6"} direction={"row"} wrap={"wrap"} justify={"space-between"} gap={"6"}>
-        <Flex direction="row" gap={"4"}>
-          <Flex direction="column" justify="between" gap={"4"}>
-            <FormControl isRequired>
-              <FormLabel htmlFor="name" fontWeight={'normal'}>
-                Collection Name
-              </FormLabel>
-              <Input
-                id="name"
-                name="name"
-                borderColor={"blackAlpha.300"}
-                focusBorderColor={"black"}
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-              />
-            </FormControl>
+        <Flex direction={"column"} gap={"2"} grow={"1"} maxW={"md"} p={"2"} rounded={"2xl"}>
+          <Heading size={"xl"} margin={"xs"}>
+            Details
+          </Heading>
+          <Text>
+            Specify some basic details about this Collection.
+          </Text>
+          <Flex direction="row" gap={"4"}>
+            <Flex direction="column" justify="between" gap={"4"}>
+              <FormControl isRequired>
+                <FormLabel htmlFor="name" fontWeight={'normal'}>
+                  Collection Name
+                </FormLabel>
+                <Input
+                  id="name"
+                  name="name"
+                  borderColor={"blackAlpha.300"}
+                  focusBorderColor={"black"}
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                />
+              </FormControl>
 
-            <FormControl isRequired>
-              <FormLabel htmlFor="owner" fontWeight={'normal'}>
-                Collection Owner
-              </FormLabel>
-              <Input
-                id="owner"
-                name="owner"
-                borderColor={"blackAlpha.300"}
-                focusBorderColor={"black"}
-                value={owner}
-                onChange={(event) => setOwner(event.target.value)}
-              />
-            </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor="owner" fontWeight={'normal'}>
+                  Collection Owner
+                </FormLabel>
+                <Input
+                  id="owner"
+                  name="owner"
+                  borderColor={"blackAlpha.300"}
+                  focusBorderColor={"black"}
+                  value={owner}
+                  onChange={(event) => setOwner(event.target.value)}
+                />
+              </FormControl>
 
-            <FormControl>
-              <FormLabel htmlFor="date" fontWeight={'normal'}>
-                Creation Date
-              </FormLabel>
-              
-              <SingleDatepicker
-                id="owner"
-                name="owner"
-                propsConfigs={{
-                  dateNavBtnProps: {
-                    colorScheme: "gray"
-                  },
-                  dayOfMonthBtnProps: {
-                    defaultBtnProps: {
-                      borderColor: "blackAlpha.300",
-                      _hover: {
+              <FormControl>
+                <FormLabel htmlFor="date" fontWeight={'normal'}>
+                  Creation Date
+                </FormLabel>
+                
+                <SingleDatepicker
+                  id="owner"
+                  name="owner"
+                  propsConfigs={{
+                    dateNavBtnProps: {
+                      colorScheme: "gray"
+                    },
+                    dayOfMonthBtnProps: {
+                      defaultBtnProps: {
+                        borderColor: "blackAlpha.300",
+                        _hover: {
+                          background: "black",
+                          color: "white",
+                        }
+                      },
+                      selectedBtnProps: {
                         background: "black",
                         color: "white",
+                      },
+                      todayBtnProps: {
+                        borderColor: "blackAlpha.300",
+                        background: "gray.50",
+                        color: "black",
                       }
                     },
-                    selectedBtnProps: {
-                      background: "black",
-                      color: "white",
-                    },
-                    todayBtnProps: {
-                      borderColor: "blackAlpha.300",
-                      background: "gray.50",
-                      color: "black",
-                    }
-                  },
-                }}
-                date={created}
-                onDateChange={setCreated}
-              />
-            </FormControl>
-          </Flex>
+                  }}
+                  date={created}
+                  onDateChange={setCreated}
+                />
+              </FormControl>
+            </Flex>
 
-          <Flex direction="column">
-            <FormControl isRequired>
-              <FormLabel htmlFor="description" fontWeight={'normal'}>
-                Description
-              </FormLabel>
-              <Textarea
-                id="description"
-                name="description"
-                value={description}
-                onChange={(event) => setDescription(event.target.value)}
-              />
-            </FormControl>
+            <Flex direction="column">
+              <FormControl isRequired>
+                <FormLabel htmlFor="description" fontWeight={'normal'}>
+                  Description
+                </FormLabel>
+                <Textarea
+                  id="description"
+                  name="description"
+                  value={description}
+                  onChange={(event) => setDescription(event.target.value)}
+                />
+              </FormControl>
+            </Flex>
           </Flex>
+        </Flex>
+
+        <Flex direction={"column"} gap={"2"} p={"4"} rounded={"2xl"} background={"whitesmoke"}>
+          <Flex align={"center"} gap={"2"}><InfoOutlineIcon boxSize={"8"} /><Heading>Collections</Heading></Flex>
+          <Text>Collections can be used to organize Entities. Any type of Entity can be included in a Collection.</Text>
+          <Text>Entities can be added and removed from a Collection after it has been created.</Text>
         </Flex>
       </Flex>
 
