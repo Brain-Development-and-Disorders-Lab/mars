3542ce030bc1cfa96f3b9624546b0e940d747379
Henry Burgess <henryjburg@gmail.com>
2022-12-09 15:57:20 -0600

    Update Entity creation start page

--- client/src/pages/Create/Entity/Start.tsx
+++ client/src/pages/Create/Entity/Start.tsx
@@ -1,145 +1,137 @@
-// React and Grommet
+// React
 import React, { useState } from "react";
-import {
-  Anchor,
-  Box,
-  Button,
-  DateInput,
-  Form,
-  FormField,
-  PageHeader,
-  TextArea,
-  TextInput,
-} from "grommet/components";
-import { Page, PageContent } from "grommet";
-import { LinkNext } from "grommet-icons";
 
 // Navigation
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 // Database and models
 import { Create } from "types";
 
 // Utility functions
 import { pseudoId } from "src/database/functions";
+import { Box, Button, Flex, FormControl, FormHelperText, FormLabel, Heading, Input, Text, Textarea } from "@chakra-ui/react";
+import { SingleDatepicker } from "chakra-dayzed-datepicker";
+import { ChevronRightIcon, CloseIcon, InfoOutlineIcon } from "@chakra-ui/icons";
 
 export const Start = ({}) => {
-  const navigate = useNavigate();
+  // Used to manage what detail inputs are presented
+  // const [pageState, setPageState] = useState("start" as "start" | "attributes" | "associations");
 
   // Extract prior state and apply
   const { state } = useLocation();
 
-  const initialName =
-    state === null ? pseudoId() : (state as Create.Entity.Associations).name;
-  const initialCreated =
-    state === null
-      ? new Date().toISOString()
-      : (state as Create.Entity.Associations).created;
-  const initialOwner =
-    state === null ? "" : (state as Create.Entity.Associations).owner;
-  const initialDescription =
-    state === null ? "" : (state as Create.Entity.Associations).description;
+  const initialName = state === null ? pseudoId() : (state as Create.Entity.Associations).name;
+  const initialCreated = state === null ? new Date().toISOString() : (state as Create.Entity.Associations).created;
+  const initialOwner = state === null ? "" : (state as Create.Entity.Associations).owner;
+  const initialDescription = state === null ? "" : (state as Create.Entity.Associations).description;
 
   const [name, setName] = useState(initialName);
-  const [created, setCreated] = useState(initialCreated);
+  const [created, setCreated] = useState(new Date(initialCreated));
   const [owner, setOwner] = useState(initialOwner);
   const [description, setDescription] = useState(initialDescription);
 
-  const startState: Create.Entity.Start = {
-    from: "none",
-    name: name,
-    created: created,
-    owner: owner,
-    description: description,
-  };
-
   return (
-    <Page kind="wide" pad={{left: "small", right: "small"}}>
-      <PageContent>
-        <PageHeader
-          title="Create an Entity: Start"
-          parent={<Anchor label="Home" href="/" />}
-        />
-        <Box width="large" fill>
-          <Form
-            onChange={() => {}}
-            onReset={() => {}}
-            onSubmit={() => {
-              startState.from = "start";
-              navigate("/create/entity/associations", {
-                state: startState,
-              });
-            }}
-          >
-            <Box direction="row" gap="medium">
-              <Box direction="column" justify="between" basis="1/3">
-                <FormField
-                  label="Name"
+    <Box m={"2"}>
+      <Flex direction={"column"} p={"2"} pt={"8"} pb={"8"} >
+        <Flex direction={"row"}>
+          <Heading size={"2xl"}>Create Entity: Start</Heading>
+        </Flex>
+      </Flex>
+
+      <Flex p={"2"} pb={"6"} direction={"row"} wrap={"wrap"} justify={"space-between"} gap={"6"}>
+        <Flex direction={"column"} gap={"2"} maxW={"2xl"} p={"2"} rounded={"2xl"}>
+          <Heading size={"xl"} margin={"xs"}>
+            Details
+          </Heading>
+          <Text>
+            Specify some basic details about this Entity.
+            Relations between Entities and membership to Collections can be specified on the following page.
+            Finally, the metadata associated with this Entity should be specified using Attributes and corresponding Parameters.
+          </Text>
+          <Flex direction={"row"} gap={"2"} grow={"1"}>
+            <Flex direction={"column"} gap={"2"}>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input
                   name="name"
-                  info="A standardised name or ID for the Entity."
-                >
-                  <TextInput
-                    name="name"
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}
-                    required
-                  />
-                </FormField>
-                <FormField
-                  label="Owner"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                  required
+                />
+                <FormHelperText>A standardised name or ID for the Entity.</FormHelperText>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Owner</FormLabel>
+                <Input
                   name="owner"
-                  info="Owner of the Entity."
-                >
-                  <TextInput
-                    name="owner"
-                    value={owner}
-                    onChange={(event) => setOwner(event.target.value)}
-                    required
-                  />
-                </FormField>
-                <FormField
-                  label="Created"
-                  name="created"
-                  info="Date the Entity was created."
-                >
-                  <DateInput
-                    format="mm/dd/yyyy"
-                    value={created}
-                    onChange={({ value }) => setCreated(value.toString())}
-                    required
-                  />
-                </FormField>
-              </Box>
+                  value={owner}
+                  onChange={(event) => setOwner(event.target.value)}
+                  required
+                />
+                <FormHelperText>Owner of the Entity.</FormHelperText>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Created</FormLabel>
+                <SingleDatepicker
+                  id="owner"
+                  name="owner"
+                  propsConfigs={{
+                    dateNavBtnProps: {
+                      colorScheme: "gray"
+                    },
+                    dayOfMonthBtnProps: {
+                      defaultBtnProps: {
+                        borderColor: "blackAlpha.300",
+                        _hover: {
+                          background: "black",
+                          color: "white",
+                        }
+                      },
+                      selectedBtnProps: {
+                        background: "black",
+                        color: "white",
+                      },
+                      todayBtnProps: {
+                        borderColor: "blackAlpha.300",
+                        background: "gray.50",
+                        color: "black",
+                      }
+                    },
+                  }}
+                  date={created}
+                  onDateChange={setCreated}
+                />
+                <FormHelperText>Date the Entity was created.</FormHelperText>
+              </FormControl>
+            </Flex>
 
-              <Box direction="column" justify="between" basis="2/3">
-                <FormField
-                  label="Description"
-                  name="description"
-                  info="A brief description of the new Entity. Most details should be inputted as Attributes with Parameters."
-                >
-                  <TextArea
-                    value={description}
-                    onChange={(event) => setDescription(event.target.value)}
-                  />
-                </FormField>
-              </Box>
-            </Box>
-
-            <Box direction="row" justify="between" margin="medium">
-              <Button label="Cancel" color="status-critical" onClick={() => navigate("/")} />
-              <Button
-                type="submit"
-                color="status-ok"
-                primary
-                label="Continue"
-                icon={<LinkNext />}
-                reverse
+            <FormControl>
+              <FormLabel>Description</FormLabel>
+              <Textarea
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
               />
-            </Box>
-          </Form>
-        </Box>
-      </PageContent>
-    </Page>
+              <FormHelperText>A brief description of the new Entity. Most details should be inputted as Attributes with Parameters.</FormHelperText>
+            </FormControl>
+          </Flex>
+        </Flex>
+
+        <Flex direction={"column"} gap={"2"} p={"4"} rounded={"2xl"} background={"whitesmoke"}>
+          <Flex align={"center"} gap={"2"}><InfoOutlineIcon boxSize={"8"} /><Heading>Entities</Heading></Flex>
+          <Text>Entity information.</Text>
+        </Flex>
+      </Flex>
+
+      {/* Action buttons */}
+      <Flex p={"2"} direction={"row"} w={"full"} flexWrap={"wrap"} gap={"6"} justify={"space-between"}>
+        <Button colorScheme={"red"} variant={"outline"} rightIcon={<CloseIcon />}>
+          Cancel
+        </Button>
+        <Button colorScheme={"green"} rightIcon={<ChevronRightIcon />}>
+          Next
+        </Button>
+      </Flex>
+    </Box>
   );
 };
 export default Start;
