2e835acf4c0252c88b9ecb690027d3f876dcc653
Henry Burgess <henryjburg@gmail.com>
2023-03-06 09:11:19 -0600

    Update Attribute display

--- client/src/components/Attribute/index.tsx
+++ client/src/components/Attribute/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Flex, Icon, Input, Textarea } from "@chakra-ui/react";
+import { Button, Card, CardBody, CardFooter, Divider, Flex, Icon, Input, Textarea } from "@chakra-ui/react";
 import { AttributeProps } from "@types";
 import ParameterGroup from "@components/ParameterGroup";
 import { CloseIcon } from "@chakra-ui/icons";
@@ -19,71 +19,64 @@ const Attribute = (props: AttributeProps) => {
   };
 
   return (
-    <Flex
-      direction={"row"}
-      align={"center"}
-      gap={"2"}
-      p={"2"}
-      bg={"gray.50"}
-      rounded={"lg"}
-    >
-      <Flex p={"2"} pb={"6"} direction={"row"} wrap={"wrap"} gap={"6"}>
-        <Flex
-          direction={"column"}
-          gap={"2"}
-          maxW={"md"}
-          p={"2"}
-          rounded={"2xl"}
-          grow={"1"}
-        >
-          <Input
-            placeholder={"Attribute Name"}
-            value={name}
-            disabled={finished}
-            onChange={(event) => setName(event.target.value)}
-          />
-          <Textarea
-            value={description}
-            placeholder={"Attribute Description"}
-            disabled={finished}
-            onChange={(event) => setDescription(event.target.value)}
-          />
-          <Flex direction={"row"} gap={"2"}>
-            <Button
-              colorScheme={"red"}
-              variant={"outline"}
-              onClick={() => {
-                if (props.onRemove) {
-                  props.onRemove(props.identifier);
-                }
-              }}
-              rightIcon={<CloseIcon />}
-            >
-              Remove
-            </Button>
-            <Button
-              rightIcon={<Icon as={AiOutlineSave} />}
-              colorScheme={"green"}
-              onClick={() => {
-                setFinished(true);
-                if (props.onUpdate) {
-                  props.onUpdate(attributeData);
-                }
-              }}
+    <Card variant={"outline"}>
+      <CardBody>
+        <Flex direction={"row"} gap={"2"}>
+          <Flex direction={"column"} gap={"2"}>
+            <Input
+              placeholder={"Attribute Name"}
+              value={name}
               disabled={finished}
-            >
-              Save
-            </Button>
+              onChange={(event) => setName(event.target.value)}
+            />
+            <Textarea
+              value={description}
+              placeholder={"Attribute Description"}
+              disabled={finished}
+              onChange={(event) => setDescription(event.target.value)}
+            />
           </Flex>
-        </Flex>
 
-        <ParameterGroup
-          parameters={parameters}
-          viewOnly={finished}
-          setParameters={setParameters}
-        />
-      </Flex>
-    </Flex>
+          <ParameterGroup
+            parameters={parameters}
+            viewOnly={finished}
+            setParameters={setParameters}
+          />
+        </Flex>
+      </CardBody>
+
+      <Divider />
+
+      <CardFooter justify={"right"}>
+        <Flex direction={"row"} gap={"2"}>
+          <Button
+            colorScheme={"red"}
+            variant={"outline"}
+            onClick={() => {
+              if (props.onRemove) {
+                props.onRemove(props.identifier);
+              }
+            }}
+            rightIcon={<CloseIcon />}
+          >
+            Remove
+          </Button>
+          <Button
+            rightIcon={<Icon as={AiOutlineSave} />}
+            colorScheme={"green"}
+            onClick={() => {
+              setFinished(true);
+              if (props.onUpdate) {
+                props.onUpdate(attributeData);
+              }
+            }}
+            disabled={finished}
+          >
+            Save
+          </Button>
+        </Flex>
+      </CardFooter>
+    </Card>
   );
 };
 
