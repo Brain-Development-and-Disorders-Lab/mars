37e8b4fec407aaad4af73c34b48180c41edfea31
Henry Burgess <henryjburg@gmail.com>
2023-04-04 14:56:57 -0500

    Refactor Search routes

--- server/src/routes/Search.ts
+++ server/src/routes/Search.ts
@@ -2,29 +2,15 @@
 import express from "express";
 
 // Database connection
-import { getDatabase } from "../database/connection";
+import { Search } from "src/operations/Search";
 
 const SearchRoute = express.Router();
 
-// Name of MongoDB collection storing Entities
-const ENTITIES_COLLECTION = "entities";
-
 // Route: Search for a specific string in the colleciton of Entities
 SearchRoute.route("/search/:query").get(async (request: any, response: any) => {
-  const database = getDatabase();
-
-  // Configure database query
-  const query = { $text: { $search: request.params.query } };
-  const sort = { score: { $meta: "textScore" } };
-
-  database
-    .collection(ENTITIES_COLLECTION)
-    .find(query)
-    .sort(sort)
-    .toArray((error, content) => {
-      if (error) throw error;
-      response.json(content);
-    });
+  Search.get(request.params.query).then((results) => {
+    response.json(results);
+  });
 });
 
 export default SearchRoute;
