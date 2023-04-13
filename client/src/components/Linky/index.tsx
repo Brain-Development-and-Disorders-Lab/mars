fb67b0b1b1990bfc13fe69e1e65d51424bab976a
Henry Burgess <henryjburg@gmail.com>
2023-04-13 13:15:10 -0500

    Update Linky component

--- client/src/components/Linky/index.tsx
+++ client/src/components/Linky/index.tsx
@@ -1,47 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { Button, Link, useToast } from "@chakra-ui/react";
+import { Button, Link } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 // Database and models
 import { getData } from "@database/functions";
 import {
-  LinkyProps,
-  Attribute,
-  Collection,
-  Entity,
+  LinkyProps
 } from "@types";
 
 const Linky = (props: LinkyProps) => {
-  const toast = useToast();
   const navigate = useNavigate();
 
-  const [linkData, setLinkData] = useState(
-    {} as Attribute | Collection | Entity
-  );
+  const [linkIsValid, setLinkIsValid] = useState(false);
+  const [linkLabel, setLinkLabel] = useState("Invalid");
 
   useEffect(() => {
     getData(`/${props.type}/${props.id}`).then((value) => {
-      setLinkData(value);
+      setLinkIsValid(true);
+      setLinkLabel(value.name);
     }).catch((_error) => {
-      toast({
-        title: "Link Error",
-        status: "error",
-        description: "Could not retrieve information for Link.",
-        duration: 4000,
-        position: "bottom-right",
-        isClosable: true,
-      });
+      setLinkIsValid(false);
+      if (props.fallback) {
+        setLinkLabel(props.fallback);
+      }
     });
   }, []);
+
+  const onClickHandler = () => {
+    if (linkIsValid) navigate(`/${props.type}/${props.id}`);
+  };
 
   return (
     <Button
       variant={"link"}
-      color={props.color ? props.color : "gray.600"}
+      color={props.color ? props.color : "gray.700"}
       as={Link}
-      onClick={() => navigate(`/${props.type}/${props.id}`)}
+      onClick={onClickHandler}
     >
-      {linkData.name}
+      {linkLabel}
     </Button>
   );
 };
