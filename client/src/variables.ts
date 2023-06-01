fc51d00cde97fd90a38e48cb271d51a25188bb70
Henry Burgess <henryjburg@gmail.com>
2023-06-01 10:33:28 -0500

    Update variables.ts

--- client/src/variables.ts
+++ client/src/variables.ts
@@ -1,3 +1,2 @@
-// export const SERVER_URL = "https://mars-server.azurewebsites.net";
 export const SERVER_URL = "http://localhost:8000";
-export const DEVELOPER_MODE = false;
+export const DEVELOPER_MODE = true;
