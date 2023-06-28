5c1108ac22c86f48cc0e19b02c88e63fe62682c7
Henry Burgess <henryjburg@gmail.com>
2023-06-27 22:34:07 -0500

    Handle JSON file import

--- server/src/operations/Server.ts
+++ server/src/operations/Server.ts
@@ -41,17 +41,74 @@ export class Server {
   };
 
   static import = (files: any): Promise<boolean> => {
-    consola.start("Importing file");
+    consola.start("Received file for import");
     return new Promise((resolve, reject) => {
       if (files.file) {
-        const uploaded = files.file;
-        consola.info("Received file:", uploaded.name);
+        const receivedFile = files.file;
+        consola.info("Received file:", receivedFile.name);
 
         // Copy file to local directory
-        uploaded.mv(`${__dirname}/${uploaded.name}`).err;
-        consola.success("Imported file:", uploaded.name);
-        resolve(true);
+        receivedFile.mv(`${__dirname}/${receivedFile.name}`).err;
+
+        const receivedFileData = receivedFile.data as Buffer;
+
+        let parsedFileData;
+        try {
+          parsedFileData = JSON.parse(receivedFileData.toString("utf-8"));
+          consola.success("Parsed JSON file");
+          consola.info("Version:", parsedFileData["timestamp"]);
+          consola.info("Entities:", parsedFileData["entities"].length);
+          consola.info("Collections:", parsedFileData["collections"].length);
+          consola.info("Attributes:", parsedFileData["attributes"].length);
+          consola.info("Activity:", parsedFileData["activity"].length, "entries");
+        } catch (error) {
+          consola.error("Error parsing JSON file");
+          reject(false);
+        }
+
+        // Import each part by checking if it exists, updating if so, otherwise creating
+        consola.start("Importing file contents");
+
+        // Import Entities
+        consola.start("Importing Entities");
+        const importedEntities = parsedFileData["entities"] as EntityModel[];
+        const entityOperations = [];
+        for (let entity of importedEntities) {
+          entityOperations.push(Entities.update(entity));
+        }
+
+        // Import Collections
+        consola.start("Importing Entities");
+        const importedCollections = parsedFileData["collections"] as CollectionModel[];
+        const collectionOperations = [];
+        for (let collection of importedCollections) {
+          collectionOperations.push(Collections.update(collection));
+        }
+
+        // Import Attributes
+        consola.start("Importing Attributes");
+        const importedAttributes = parsedFileData["attributes"] as AttributeModel[];
+        const attributeOperations = [];
+        for (let attribute of importedAttributes) {
+          attributeOperations.push(Attributes.update(attribute));
+        }
+
+        Promise.all([
+          Promise.all(entityOperations),
+          Promise.all(attributeOperations),
+          Promise.all(collectionOperations),
+        ]).then((results: [EntityModel[], AttributeModel[], CollectionModel[]]) => {
+          consola.success("Imported", results[0].length, "Entities");
+          consola.success("Imported", results[1].length, "Attributes");
+          consola.success("Imported", results[2].length, "Collections");
+          consola.success("Imported file:", receivedFile.name);
+          resolve(true);
+        }).catch((_error) => {
+          consola.error("Error importing file");
+          reject(false);
+        });
       } else {
+        consola.error("Error importing file");
         reject(false);
       }
     });
