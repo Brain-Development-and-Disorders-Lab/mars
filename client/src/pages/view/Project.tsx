674845172c99cde20a94f939d47aa36ceeb87d7f
Henry Burgess <henryjburg@gmail.com>
2023-08-21 21:46:15 -0500

    RBIO-58 Partial migrate Dashboard to Project

--- client/src/pages/view/Project.tsx
+++ client/src/pages/view/Project.tsx
@@ -1,4 +1,3 @@
-// React
 import React, { useEffect, useState } from "react";
 
 // Existing and custom components
@@ -6,56 +5,56 @@ import {
   Button,
   Flex,
   Heading,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
-  Table,
-  TableContainer,
-  Tag,
-  TagLabel,
-  Tbody,
-  Td,
-  Textarea,
-  Th,
-  Thead,
-  Tr,
+  Text,
   useToast,
+  Spacer,
+  List,
+  ListItem,
 } from "@chakra-ui/react";
 import { Content } from "@components/Container";
+import DataTable from "@components/DataTable";
 import Icon from "@components/Icon";
+import Linky from "@components/Linky";
 
 // Existing and custom types
-import { ProjectModel } from "@types";
+import { ActivityModel, ProjectModel } from "@types";
 
 // Utility functions and libraries
-import { deleteData, getData, postData } from "@database/functions";
+import { getData } from "src/database/functions";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 import _ from "lodash";
 
 // Routing and navigation
 import { useNavigate, useParams } from "react-router-dom";
 
 const Project = () => {
-  const { id } = useParams();
-  const toast = useToast();
+  // Enable navigation
   const navigate = useNavigate();
+  const { id } = useParams();
 
+  // Toast to show errors
+  const toast = useToast();
+
+  // Page state
   const [isLoaded, setIsLoaded] = useState(false);
-  const [editing, setEditing] = useState(false);
   const [isError, setIsError] = useState(false);
 
+  // Page data
   const [projectData, setProjectData] = useState({} as ProjectModel);
-  const [projectDescription, setProjectDescription] = useState("");
+  const [projectEntities, setProjectEntities] = useState([] as string[]);
+  const [projectCollections, setProjectCollections] = useState([] as string[]);
+  const [projectActivity, setProjectActivity] = useState([] as ActivityModel[]);
 
+  // Get all Project data
   useEffect(() => {
-    // Populate Project data
     getData(`/projects/${id}`)
-      .then((response: ProjectModel) => {
-        setProjectData(response);
-        setProjectDescription(response.description);
+      .then((result: ProjectModel) => {
+        setProjectData(result);
+        setProjectEntities(result.entities);
+        setProjectCollections(result.collections);
+        setProjectActivity(result.activity);
       })
       .catch((_error) => {
         toast({
@@ -71,225 +70,283 @@ const Project = () => {
       .finally(() => {
         setIsLoaded(true);
       });
-  }, [id, isLoaded]);
+  }, []);
 
-  // Delete the Project when confirmed
-  const handleDeleteClick = () => {
-    // Update data
-    deleteData(`/projects/${id}`)
-      .then((_response) => {
-        toast({
-          title: "Deleted!",
-          status: "success",
-          duration: 2000,
-          position: "bottom-right",
-          isClosable: true,
-        });
-      })
-      .catch(() => {
-        toast({
-          title: "Error",
-          description: `An error occurred when deleting Project "${projectData.name}".`,
-          status: "error",
-          duration: 2000,
-          position: "bottom-right",
-          isClosable: true,
-        });
-      })
-      .finally(() => {
-        setEditing(false);
-        navigate("/projects");
-      });
-  };
+  // Configure Entity table
+  const entityTableData: string[] = projectEntities;
+  const entityTableColumns = [
+    {
+      id: (info: any) => info.row.original._id,
+      cell: (info: any) => (
+        <Linky
+          id={info.row.original._id}
+          type={"entities"}
+          fallback={info.row.original._id}
+        />
+      ),
+    header: "Name",
+    },
+    {
+      id: (info: any) => `n_${info.row.original._id}`,
+      cell: (info: any) => {
+        return (
+          <Flex justifyContent={"right"}>
+            <Button
+              key={`view-entity-${info.getValue()}`}
+              colorScheme={"blackAlpha"}
+              rightIcon={<Icon name={"c_right"} />}
+              onClick={() => navigate(`/entities/${info.getValue()}`)}
+            >
+              View
+            </Button>
+          </Flex>
+        );
+      },
+      header: "",
+    },
+  ];
 
-  /**
-   * Handle the edit button being clicked
-   */
-  const handleEditClick = () => {
-    if (editing) {
-      const updateData: ProjectModel = {
-        _id: projectData._id,
-        name: projectData.name,
-        description: projectDescription,
-        created: projectData.created,
-        owner: projectData.owner,
-        users: projectData.users,
-        entities: projectData.entities,
-        collections: projectData.collections,
-        attributes: projectData.attributes,
-        history: projectData.history,
-      };
-
-      // Update data
-      postData(`/projects/update`, updateData)
-        .then((_response) => {
-          toast({
-            title: "Saved!",
-            status: "success",
-            duration: 2000,
-            position: "bottom-right",
-            isClosable: true,
-          });
-        })
-        .catch(() => {
-          toast({
-            title: "Error",
-            description: "An error occurred when saving updates.",
-            status: "error",
-            duration: 2000,
-            position: "bottom-right",
-            isClosable: true,
-          });
-        })
-        .finally(() => {
-          setEditing(false);
-        });
-    } else {
-      setEditing(true);
-    }
-  };
+  // Configure Collections table
+  const collectionTableData: string[] = projectCollections;
+  const collectionTableColumns = [
+    {
+      id: (info: any) => info.row.original._id,
+      cell: (info: any) => (
+        <Linky
+          id={info.row.original._id}
+          type={"entities"}
+          fallback={info.row.original._id}
+        />
+      ),
+    header: "Name",
+    },
+    {
+      id: (info: any) => `n_${info.row.original._id}`,
+      cell: (info: any) => {
+        return (
+          <Flex justifyContent={"right"}>
+            <Button
+              key={`view-entity-${info.getValue()}`}
+              colorScheme={"blackAlpha"}
+              rightIcon={<Icon name={"c_right"} />}
+              onClick={() => navigate(`/entities/${info.getValue()}`)}
+            >
+              View
+            </Button>
+          </Flex>
+        );
+      },
+      header: "",
+    },
+  ];
 
   return (
     <Content isError={isError} isLoaded={isLoaded}>
-      <Flex direction={"column"} gap={"4"}>
-        <Flex
-          gap={"4"}
-          p={"4"}
-          direction={"row"}
-          justify={"space-between"}
-          align={"center"}
-          wrap={"wrap"}
-        >
+      <Flex direction={"row"} wrap={"wrap"} gap={"4"} p={"4"} h={"100%"}>
+        <Flex direction={"column"} gap={"4"} grow={"2"} h={"100%"}>
           <Flex
-            align={"center"}
             gap={"4"}
-            shadow={"lg"}
-            p={"2"}
-            border={"2px"}
-            rounded={"md"}
+            p={"4"}
+            direction={"row"}
+            justify={"space-between"}
+            align={"center"}
+            wrap={"wrap"}
           >
-            <Icon name={"project"} size={"lg"} />
-            <Heading fontWeight={"semibold"}>
-              Project: {projectData.name}
-            </Heading>
+            <Flex
+              align={"center"}
+              gap={"4"}
+              shadow={"lg"}
+              p={"2"}
+              border={"2px"}
+              rounded={"md"}
+            >
+              <Icon
+                name={"project"}
+                size={"lg"}
+              />
+              <Heading fontWeight={"semibold"}>{projectData.name}</Heading>
+            </Flex>
+          </Flex>
+          {/* Collections and Entities */}
+          <Flex
+            h={"50%"}
+            direction={"column"}
+            p={"4"}
+            background={"white"}
+            rounded={"md"}
+            gap={"2"}
+            border={"1px"}
+            borderColor={"gray.100"}
+          >
+            {/* Collections heading */}
+            <Flex direction={"row"} align={"center"} gap={"4"}>
+              <Icon name={"collection"} size={"lg"} />
+              <Heading fontWeight={"semibold"}>Collections</Heading>
+            </Flex>
+
+            {/* Collections table */}
+            {projectCollections && projectCollections.length > 0 ? (
+                <DataTable
+                  columns={collectionTableColumns}
+                  data={collectionTableData}
+                  visibleColumns={{}}
+                  hidePagination
+                  hideSelection
+                />
+            ) : (
+              <Text>There are no Collections to display.</Text>
+            )}
+
+            <Spacer />
+
+            <Flex justify={"right"}>
+              <Button
+                key={`view-collection-all`}
+                colorScheme={"teal"}
+                rightIcon={<Icon name={"c_right"} />}
+                onClick={() => navigate(`/collections`)}
+              >
+                View All
+              </Button>
+            </Flex>
           </Flex>
 
-          {/* Buttons */}
           <Flex
-            direction={"row"}
-            gap={"4"}
-            wrap={"wrap"}
+            h={"50%"}
+            direction={"column"}
             p={"4"}
+            background={"white"}
             rounded={"md"}
+            gap={"2"}
+            border={"1px"}
+            borderColor={"gray.100"}
           >
-            {editing && (
-              <Popover>
-                <PopoverTrigger>
-                  <Button
-                    colorScheme={"red"}
-                    rightIcon={<Icon name={"delete"} />}
-                  >
-                    Delete
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                  <PopoverArrow />
-                  <PopoverCloseButton />
-                  <PopoverHeader>Confirmation</PopoverHeader>
-                  <PopoverBody>
-                    Are you sure you want to delete this Project?
-                    This will destroy all associated Entities, Collections, and Attributes.
-                    <Flex direction={"row"} p={"2"} justify={"center"}>
-                      <Button
-                        colorScheme={"green"}
-                        rightIcon={<Icon name={"check"} />}
-                        onClick={handleDeleteClick}
-                      >
-                        Confirm
-                      </Button>
-                    </Flex>
-                  </PopoverBody>
-                </PopoverContent>
-              </Popover>
+            {/* Entities heading */}
+            <Flex direction={"row"} align={"center"} gap={"4"}>
+              <Icon name={"entity"} size={"lg"} />
+              <Heading fontWeight={"semibold"}>Entities</Heading>
+            </Flex>
+
+            {/* Entities table */}
+            {projectEntities && projectEntities.length > 0 ? (
+              <DataTable
+                columns={entityTableColumns}
+                data={entityTableData}
+                visibleColumns={{}}
+                hidePagination
+                hideSelection
+              />
+            ) : (
+              <Text>There are no Entities to display.</Text>
             )}
-            <Button
-              colorScheme={editing ? "green" : "gray"}
-              rightIcon={
-                editing ? <Icon name={"check"} /> : <Icon name={"edit"} />
-              }
-              onClick={handleEditClick}
-            >
-              {editing ? "Done" : "Edit"}
-            </Button>
+
+            <Spacer />
+
+            <Flex justify={"right"}>
+              <Button
+                key={`view-entity-all`}
+                colorScheme={"teal"}
+                rightIcon={<Icon name={"c_right"} />}
+                onClick={() => navigate(`/entities`)}
+              >
+                View All
+              </Button>
+            </Flex>
           </Flex>
         </Flex>
 
-        <Flex direction={"row"} gap={"4"} p={"4"} wrap={"wrap"}>
+        {/* Activity */}
+        <Flex
+          direction={"column"}
+          gap={"4"}
+          grow={"1"}
+          h={"100%"}
+          rounded={"md"}
+          border={"1px"}
+          borderColor={"gray.100"}
+        >
           <Flex
+            background={"white"}
             direction={"column"}
+            rounded={"md"}
+            h={"fit-content"}
             p={"4"}
             gap={"2"}
-            grow={"1"}
-            h={"fit-content"}
-            rounded={"md"}
-            border={"1px"}
-            borderColor={"gray.100"}
           >
-            {/* Details */}
-            <Heading fontWeight={"semibold"} size={"lg"}>
-              Details
-            </Heading>
+            {/* Activity heading */}
+            <Flex align={"center"} gap={"4"} p={"2"}>
+              <Icon name={"activity"} size={"lg"} />
+              <Heading fontWeight={"semibold"}>Activity</Heading>
+            </Flex>
 
-            <TableContainer>
-              <Table variant={"simple"} colorScheme={"blackAlpha"}>
-                <Thead>
-                  <Tr>
-                    <Th>Field</Th>
-                    <Th>Value</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  <Tr>
-                    <Td>Owner</Td>
-                    <Td>
-                      <Tag
-                        size={"md"}
-                        key={`owner-${projectData._id}`}
-                        colorScheme={"green"}
+            {/* Activity list */}
+            <List>
+              {projectActivity && projectActivity.length > 0 ? (
+                projectData.activity.slice(0, 10).map((activity) => {
+                  // Configure the badge
+                  let operationBadgeColor = "green.400";
+                  let operationIcon = <Icon name={"entity"} color={"white"} />;
+
+                  switch (activity.type) {
+                    case "create":
+                      operationBadgeColor = "green.400";
+                      operationIcon = <Icon name={"add"} color={"white"} />;
+                      break;
+                    case "update":
+                      operationBadgeColor = "blue.400";
+                      operationIcon = <Icon name={"edit"} color={"white"} />;
+                      break;
+                    case "delete":
+                      operationBadgeColor = "red.400";
+                      operationIcon = <Icon name={"delete"} color={"white"} />;
+                      break;
+                  }
+
+                  return (
+                    <ListItem key={`activity-${activity._id}`}>
+                      <Flex
+                        direction={"row"}
+                        p={"2"}
+                        gap={"2"}
+                        mt={"2"}
+                        mb={"2"}
+                        align={"center"}
+                        background={"white"}
+                        rounded={"md"}
+                        border={"2px"}
+                        borderColor={"gray.100"}
                       >
-                        <TagLabel>{projectData.owner}</TagLabel>
-                      </Tag>
-                    </Td>
-                  </Tr>
-                  <Tr>
-                    <Td>Description</Td>
-                    <Td>
-                      <Textarea
-                        value={projectDescription}
-                        onChange={(event) => {
-                          setProjectDescription(event.target.value);
-                        }}
-                        disabled={!editing}
-                      />
-                    </Td>
-                  </Tr>
-                </Tbody>
-              </Table>
-            </TableContainer>
-          </Flex>
-          <Flex
-            direction={"column"}
-            p={"4"}
-            gap={"2"}
-            grow={"1"}
-            h={"fit-content"}
-            rounded={"md"}
-            border={"1px"}
-            borderColor={"gray.100"}
-          >
-            <Heading fontWeight={"semibold"} size={"lg"}>
-              Values
-            </Heading>
+                        <Flex
+                          rounded={"full"}
+                          bg={operationBadgeColor}
+                          p={"1.5"}
+                        >
+                          {operationIcon}
+                        </Flex>
+
+                        <Text display={{ base: "none", sm: "block" }}>
+                          {activity.details}
+                        </Text>
+
+                        <Linky
+                          id={activity.target.id}
+                          type={activity.target.type}
+                          fallback={activity.target.name}
+                        />
+
+                        <Spacer />
+
+                        <Text color={"gray.400"}>
+                          {dayjs(activity.timestamp).fromNow()}
+                        </Text>
+                      </Flex>
+                    </ListItem>
+                  );
+                })
+              ) : (
+                <Text fontSize={"md"}>No recent activity to show.</Text>
+              )}
+            </List>
           </Flex>
         </Flex>
       </Flex>
