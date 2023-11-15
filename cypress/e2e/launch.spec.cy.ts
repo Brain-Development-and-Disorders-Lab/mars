7927cbfa27c62361f570b89102859a016489e5bf
Robin Fievet <robin.fievet@gatech.edu>
2023-11-15 09:52:16 -0500

    improve UX test and use artifact on git actions

--- cypress/e2e/launch.spec.cy.ts
+++ cypress/e2e/launch.spec.cy.ts
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('checking app does launch ', () => {
+  it('checking Project and Entities box', () => {
     cy.visit('http://localhost:8080/');
+    cy.get(':nth-child(1) > .css-r4opcp > .chakra-heading').should('have.text', 'Projects');
+    cy.get(':nth-child(2) > .css-r4opcp > .chakra-heading').should('have.text', 'Entities');
   })
 })
