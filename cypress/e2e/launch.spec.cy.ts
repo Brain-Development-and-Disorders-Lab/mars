b178f25bf4a5458afbcb374e68d22a78ed121316
Robin Fievet <robin.fievet@gatech.edu>
2023-11-16 15:57:07 -0500

    minor cleanups

--- cypress/e2e/launch.spec.cy.ts
+++ cypress/e2e/launch.spec.cy.ts
@@ -1,10 +1,4 @@
 describe('checking app does launch ', () => {
-  it('checking backend exist', () => {
-    cy.visit('http://localhost:8000/');
-  })
-  it('checking mongoDB exist', () => {
-    cy.visit('http://localhost:27017/');
-  })
   it('checking Project and Entities box', () => {
     cy.visit('http://localhost:8080/');
     cy.get(':nth-child(1) > .css-r4opcp > .chakra-heading').should('have.text', 'Projects');
