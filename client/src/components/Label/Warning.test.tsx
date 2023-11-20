36e34d77ef011cb9fd26450a120e7a4ad6fb228d
Robin Fievet <robin.fievet@gatech.edu>
2023-11-20 11:36:47 -0500

    cleanup test

--- client/src/components/Label/Warning.test.tsx
+++ client/src/components/Label/Warning.test.tsx
@@ -1,6 +1,6 @@
 // Warning.test.tsx
 
-import React, { ReactElement } from 'react';
+import React from 'react';
 // import { render, screen } from '@testing-library/react';
 import { Warning } from './index';
 import { shallow } from 'enzyme';
@@ -8,7 +8,6 @@ import { shallow } from 'enzyme';
 describe('Warning Component', () => {
   it('renders with provided text', () => {
     const testText = 'Test Warning Message';
-    // const element: ReactElement = <Warning text={testText} />;
     const wrapper = shallow(<Warning text={testText} />);
 
     expect(wrapper.text()).toContain(testText);
