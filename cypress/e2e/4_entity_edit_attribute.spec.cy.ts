55135f747526d1e46dd2ea156f3dc57c32f101e8
Robin Fievet <robin.fievet@gatech.edu>
2024-01-02 10:51:54 -0500

    cleanup test

--- cypress/e2e/4_entity_edit_attribute.spec.cy.ts
+++ cypress/e2e/4_entity_edit_attribute.spec.cy.ts
@@ -3,24 +3,17 @@ describe('In entity page, edit attribute', () => {
   it('should be able to add and edit attribute', () => {
     cy.visit('http://localhost:8080/');
     cy.contains('button', 'Dashboard').click();
-
     cy.get('button').contains('View').eq(-1).click();
     cy.get('button').contains('View').eq(0).click();
     cy.contains('No Attributes.').should('exist');
-
     cy.contains('button', 'Edit').click();
 
     // add attribute
     cy.get(':nth-child(2) > .css-sdf56e > .css-1ialerq > .chakra-button').click();
     cy.get('#formName').type('Attribute Name');
     cy.get('#formDescription').type('Attribute Description');
-
-    // cy.get('button').contains('Add').eq(4).click();
     cy.get('.add-value-button-form').click();
-    // cy.contains('button', 'Add').click();
-    
     cy.get('#add-value-button-text').click();
-
     cy.get('input').eq(-2).type('Value Name');
     cy.get('input').eq(-1).type('Value Data');
     cy.get('input').eq(-1).click();
@@ -46,7 +39,6 @@ describe('In entity page, edit attribute', () => {
     cy.contains('No Attributes.').should('exist');
     cy.reload();
     cy.contains('No Attributes.').should('exist');
-
 
   });
 });
