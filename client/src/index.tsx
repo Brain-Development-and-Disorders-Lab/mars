ee8a1924bc85b96a8ef8abb8ffc1701ed3f2aaeb
Henry Burgess <henryjburg@gmail.com>
2024-05-08 20:41:05 -0500

    MARS-1004 Add Apollo to client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+// Apollo imports
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 // Logging
 import { consola } from "consola";
 consola.wrapConsole();
@@ -9,7 +12,17 @@ consola.wrapConsole();
 // Application
 import App from "./App";
 
+// Setup Apollo client
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
 // Render the application
 const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
