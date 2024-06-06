5b5331c80f4d3a8f7f2d52b6622a60e6734a01bc
Henry Burgess <henryjburg@gmail.com>
2024-06-06 11:30:36 -0500

    MARS-1006 Create new `request` function

--- client/src/database/functions.ts
+++ client/src/database/functions.ts
@@ -1,13 +1,99 @@
 // Utility functions and libraries
 import consola from "consola";
 import _ from "lodash";
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+// Custom types
+import { RequestMethod, ServerResponse } from "@types";
 
 // Get the URL of the database
 import { SERVER_URL, TOKEN_KEY } from "src/variables";
 
 // Token for request authorization
 import { getToken } from "src/util";
+
+export const request = async (
+  type: RequestMethod,
+  path: string,
+  data?: any,
+  options?: AxiosRequestConfig,
+): Promise<ServerResponse> => {
+  // Merge in options if specified
+  const requestOptions: AxiosRequestConfig = {
+    ...options,
+  };
+
+  // Configure authorization
+  if (!_.isUndefined(getToken(TOKEN_KEY))) {
+    requestOptions.headers = {
+      id_token: getToken(TOKEN_KEY).id_token,
+      ...requestOptions.headers,
+    };
+  }
+
+  // Execute request and store response if successful
+  let response: AxiosResponse;
+  switch (type) {
+    case "GET":
+      try {
+        response = await axios.get(`${SERVER_URL}${path}`, requestOptions);
+      } catch {
+        return {
+          success: false,
+          message: "Error while making request, check connectivity",
+          data: {},
+        };
+      }
+      if (!response) {
+        return {
+          success: false,
+          message: "No response received from server",
+          data: {},
+        };
+      }
+      break;
+    case "POST":
+      // POST request
+      try {
+        response = await axios.post(
+          `${SERVER_URL}${path}`,
+          data,
+          requestOptions,
+        );
+      } catch {
+        return {
+          success: false,
+          message: "Error while making request, check connectivity",
+          data: {},
+        };
+      }
+      const contentTypeHeader = response.headers["content-type"];
+      if (_.isNull(contentTypeHeader)) {
+        return {
+          success: false,
+          message: "Invalid response received from server",
+          data: {},
+        };
+      } else if (
+        _.startsWith(contentTypeHeader, "application/json") &&
+        !_.isEqual(response.statusText, "OK")
+      ) {
+        return {
+          success: false,
+          message: "Invalid JSON response received from server",
+          data: {},
+        };
+      }
+      break;
+  }
+
+  // Return an object containing the response data and status
+  return {
+    success: true,
+    message: "Recieved response from server",
+    data: response.data,
+  };
+};
 
 /**
  * Get data from the Lab API using the JavaScript `fetch` API
