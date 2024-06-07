a5b6e05019c705883246956481e81dd3a4ecb7e5
Henry Burgess <henryjburg@gmail.com>
2024-06-07 14:47:39 -0500

    MARS-1010 Remove `postData` function

--- client/src/database/functions.ts
+++ client/src/database/functions.ts
@@ -102,52 +102,6 @@ export const request = async <T>(
 };
 
 /**
- * Post data to the Lab API using the JavaScript `fetch` API
- * @param {string} path exact API path to post data to
- * @param {any} data the data to be posted to Lab
- */
-export const postData = async (
-  path: string,
-  data: any,
-  options?: AxiosRequestConfig,
-): Promise<any> => {
-  // Configure authorization
-  const requestOptions: AxiosRequestConfig = {
-    ...options,
-  };
-
-  if (!_.isUndefined(getToken(TOKEN_KEY))) {
-    requestOptions.headers = {
-      id_token: getToken(TOKEN_KEY).id_token,
-      ...requestOptions.headers,
-    };
-  }
-
-  return new Promise((resolve, reject) => {
-    axios
-      .post(`${SERVER_URL}${path}`, data, requestOptions)
-      .then((response) => {
-        const contentType = response.headers["content-type"];
-        if (_.isNull(contentType)) {
-          reject("Invalid response");
-        } else if (_.startsWith(contentType, "application/json")) {
-          if (!_.isEqual(response.statusText, "OK")) {
-            reject("Invalid response");
-          } else {
-            resolve(response.data);
-          }
-        } else {
-          resolve(response.data);
-        }
-      })
-      .catch((error) => {
-        consola.error("POST:", path);
-        reject(error);
-      });
-  });
-};
-
-/**
  * Delete data to the Lab API using the JavaScript `fetch` API
  * @param {string} path the path of the database objected to be deleted
  */
