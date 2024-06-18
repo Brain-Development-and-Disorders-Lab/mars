4a4afba69a997c20c150e99928539de14e7f4e36
Henry Burgess <henryjburg@gmail.com>
2024-06-17 21:18:06 -0500

    MARS-1004 Fix bug returning `Activity`

--- server/src/resolvers/Activity.ts
+++ server/src/resolvers/Activity.ts
@@ -5,8 +5,8 @@ export const ActivityResolvers = {
   Query: {
     // Retrieve all Activity
     activity: async (_parent: any, args: { limit: 100 }) => {
-      const allActivity = await Activity.all();
-      return allActivity.slice(0, args.limit).reverse();
+      const allActivity = (await Activity.all()).reverse();
+      return allActivity.slice(0, args.limit);
     },
   },
 
