a1dadaf0d6106333b52ee892b18c9f39eabe695a
Henry Burgess <henryjburg@gmail.com>
2024-08-08 16:04:19 -0500

    MARS-1018 Update `SearchQueryValue`

--- client/src/components/SearchQueryValue/index.tsx
+++ client/src/components/SearchQueryValue/index.tsx
@@ -1,34 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { Input, Button, VStack, Flex } from "@chakra-ui/react";
+import { Input, Button, Flex, Text, useToast } from "@chakra-ui/react";
 
+import { ValueEditorProps } from "react-querybuilder";
 import { EntityModel } from "@types";
 
 // Utility imports
 import { debounce } from "lodash";
-import { request } from "@database/functions";
-import { ValueEditorProps } from "react-querybuilder";
 import _ from "lodash";
+import { gql, useLazyQuery } from "@apollo/client";
 
 const SearchQueryValue = ({
   field,
   value,
   handleOnChange,
 }: ValueEditorProps) => {
+  // Query to search by text value
+  const SEARCH_TEXT = gql`
+    query Search($query: String, $isBuilder: Boolean, $limit: Int) {
+      search(query: $query, isBuilder: $isBuilder, limit: $limit) {
+        _id
+        name
+        description
+      }
+    }
+  `;
+  const [searchText, { loading, error }] = useLazyQuery(SEARCH_TEXT);
+
   const [inputValue, setInputValue] = useState(value || "");
-  const [searchResults, setSearchResults] = useState([] as EntityModel[]);
+  const [results, setResults] = useState([] as EntityModel[]);
+  const [showResults, setShowResults] = useState(false);
+
+  const toast = useToast();
 
   // Function to fetch entities based on the search term
   const fetchEntities = debounce(async (query) => {
     if (field === "origins" || field === "products") {
-      const response = await request<EntityModel[]>(
-        "POST",
-        "/entities/searchByTerm",
-        { query },
-      );
-      if (response.success) {
-        setSearchResults(response.data || []);
+      const results = await searchText({
+        variables: {
+          query: query,
+          isBuilder: false,
+          limit: 100,
+        },
+      });
+
+      if (results.data.search) {
+        setResults(results.data.search);
       } else {
-        setSearchResults([]);
+        setResults([]);
+      }
+
+      if (error) {
+        toast({
+          title: "Error",
+          status: "error",
+          description: error.message,
+          duration: 4000,
+          position: "bottom-right",
+          isClosable: true,
+        });
+      } else {
+        setShowResults(true);
       }
     }
   }, 300);
@@ -41,7 +72,7 @@ const SearchQueryValue = ({
     ) {
       fetchEntities(inputValue);
     } else {
-      setSearchResults([]);
+      setResults([]);
     }
   }, [inputValue, field]);
 
@@ -57,7 +88,8 @@ const SearchQueryValue = ({
   const handleSelectEntity = (entity: any) => {
     handleOnChange(entity._id);
     setInputValue(entity.name);
-    setSearchResults([]);
+    setResults([]);
+    setShowResults(false);
   };
 
   return (
@@ -70,30 +102,42 @@ const SearchQueryValue = ({
         backgroundColor={"white"}
         data-testid="value-editor"
       />
-      {searchResults.length > 0 && (
-        <VStack
-          gap={2}
-          w={"100%"} // Ensures the stack takes full width of the parent
-          p={2} // Adds padding around the stack for better spacing
-          bg={"gray.50"} // Sets a light background for the list
-          borderRadius={"md"} // Rounds the corners
+      {showResults && (
+        <Flex
+          w={"100%"}
+          p={"2"}
+          mt={"10"}
+          gap={"2"}
+          direction={"column"}
+          bg={"gray.50"}
+          borderRadius={"md"}
+          position={"absolute"}
+          maxW={"200px"}
         >
-          <Flex w={"full"} p={2} textAlign={"center"} color={"gray.600"}>
-            Please select an Entity:
+          <Text fontSize={"sm"} fontWeight={"semibold"}>
+            Results: {results.length}
+          </Text>
+          <Flex
+            direction={"column"}
+            maxH={"200px"}
+            overflowY={"auto"}
+            gap={"2"}
+            p={"0"}
+          >
+            {results.map((entity: any) => (
+              <Flex key={`e_${entity._id}`}>
+                <Button
+                  key={entity._id}
+                  onClick={() => handleSelectEntity(entity)}
+                  width={"full"}
+                  isDisabled={loading}
+                >
+                  {entity.name}
+                </Button>
+              </Flex>
+            ))}
           </Flex>
-          {searchResults.map((entity: any) => (
-            <Button
-              key={entity._id}
-              onClick={() => handleSelectEntity(entity)}
-              variant={"ghost"}
-              justifyContent={"start"} // Aligns text to the left
-              width="full" // Button takes full width of its container
-              _hover={{ bg: "gray.100" }} // Changes background on hover for better interaction
-            >
-              {entity.name}
-            </Button>
-          ))}
-        </VStack>
+        </Flex>
       )}
     </Flex>
   );
