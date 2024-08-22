1d81157c5d4f4543e538a3c6d9feeed0f8c4a1d3
Henry Burgess <henryjburg@gmail.com>
2024-08-22 15:50:50 -0500

    MARS-1013 Fix ESLint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,14 +2,6 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  ignore: [
-    ".eslintrc.js",
-    "gulpfile.js",
-    "jest.config.js",
-    "webpack.*.js",
-    "node_modules/",
-    "*.d.ts",
-  ],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
