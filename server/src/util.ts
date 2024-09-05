25ee31a240899f5139cbc37774a9e9f179bdaa10
Henry Burgess <henryjburg@gmail.com>
2024-09-04 23:05:19 -0500

    MARS-1029 Move to 9-char IDs

--- server/src/util.ts
+++ server/src/util.ts
@@ -44,5 +44,5 @@ export const authenticate = (request: any, response: any, next: () => void) => {
 export const getIdentifier = (
   type: "entity" | "attribute" | "activity" | "project" | "workspace",
 ): string => {
-  return `${type.slice(0, 1)}${nanoid(7)}`;
+  return `${type.slice(0, 1)}${nanoid(9)}`;
 };
