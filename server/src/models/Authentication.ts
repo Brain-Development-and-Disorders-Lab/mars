242f93f54c08639549847cd0237d938aee173d3f
Henry Burgess <henryjburg@gmail.com>
2024-09-11 14:45:54 -0500

    MARS-1027 Create new User entries automatically

--- server/src/models/Authentication.ts
+++ server/src/models/Authentication.ts
@@ -29,9 +29,23 @@ export class Authentication {
       redirect_uri: REDIRECT_URI,
     }).toString();
 
-    // If non-production, resolve with test user
-    if (process.env.NODE_ENV !== "production") {
-      return {
+    // Generate the authentication payload
+    let authenticationPayload: IAuth;
+    if (process.env.NODE_ENV === "production") {
+      // If production, obtain the ORCiD token data
+      const response: AxiosResponse = await axios.post(TOKEN_URL, loginData, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+      if (response.status !== 200) {
+        throw new Error("Invalid");
+      }
+      authenticationPayload = response.data;
+    } else {
+      // If non-production, resolve with test user
+      authenticationPayload = {
         orcid: "XXXX-0000-DEMO-1111",
         name: "Test User",
         token: "test_token_value",
@@ -39,31 +53,15 @@ export class Authentication {
       };
     }
 
-    const response: AxiosResponse = await axios.post(TOKEN_URL, loginData, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
-
-    if (response.status !== 200) {
-      throw new Error("Invalid");
-    }
-
-    const authenticationPayload: IAuth = response.data;
-
+    // Check if the User exists, and create a new User if not
     const exists = await Users.exists(authenticationPayload.orcid);
-    if (exists) {
-      await Users.update({
-        _id: authenticationPayload.orcid,
-        name: authenticationPayload.name,
-        token: authenticationPayload.token,
-      });
-    } else {
+    if (!exists) {
       await Users.create({
         _id: authenticationPayload.orcid,
         name: authenticationPayload.name,
+        email: "",
         token: authenticationPayload.token,
+        workspaces: [],
       });
     }
 
@@ -71,35 +69,35 @@ export class Authentication {
   };
 
   static validate = async (token: string): Promise<UserModel> => {
-    if (process.env.NODE_ENV !== "production") {
-      return {
-        _id: "XXXX-0000-DEMO-1111",
-        name: "Test User",
-        email: "mars@reusable.bio",
-        token: "test_token_value",
-      };
+    if (process.env.NODE_ENV === "production") {
+      // Validate the token and obtain the ORCiD
+      const client = new JwksClient({
+        jwksUri: "https://orcid.org/oauth/jwks",
+        requestHeaders: {},
+        timeout: 30000,
+      });
+
+      const result = await client.getSigningKey(
+        "production-orcid-org-7hdmdswarosg3gjujo8agwtazgkp1ojs",
+      );
+      const orcid = verify(token, result.getPublicKey()).sub;
+
+      if (_.isUndefined(orcid)) {
+        throw new Error("ORCiD could not be retrieved from login token");
+      }
+
+      const user = await Users.getOne(orcid.toString());
+      if (user) {
+        return user;
+      }
+    } else {
+      // Return the test user account
+      const user = await Users.getOne("XXXX-0000-DEMO-1111");
+      if (user) {
+        return user;
+      }
     }
 
-    const client = new JwksClient({
-      jwksUri: "https://orcid.org/oauth/jwks",
-      requestHeaders: {},
-      timeout: 30000,
-    });
-
-    const result = await client.getSigningKey(
-      "production-orcid-org-7hdmdswarosg3gjujo8agwtazgkp1ojs",
-    );
-    const orcid = verify(token, result.getPublicKey()).sub;
-
-    if (_.isUndefined(orcid)) {
-      throw new Error("ORCiD could not be retrieved from login token");
-    }
-
-    const user = await Users.getOne(orcid.toString());
-    if (user) {
-      return user;
-    }
-
-    throw new Error(`User not found with ORCID: ${orcid.toString()}`);
+    throw new Error(`Could not validate token`);
   };
 }
