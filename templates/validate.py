68e10de54c396a9719a3c4f02590dbdd976739e3
Henry Burgess <henryjburg@gmail.com>
2024-09-25 12:26:58 -0500

    MARS-1047 Resolve missing fields, export data

--- templates/validate.py
+++ templates/validate.py
@@ -1,6 +1,7 @@
 # Script to parse an exported collection of Entities and validate format and contents
 import json
 import logging
+import nanoid
 
 
 # Setup logging
@@ -9,6 +10,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Variables
 COLLECTION_PATH = "/Users/henryburgess/Downloads/metadata.entities.json"
+GENERATE_OUTPUT = True
+OUTPUT_PATH = "/Users/henryburgess/Downloads/metadata.entities.cleaned.json"
 ENTITY_FIELDS = [
   "_id",
   "timestamp",
@@ -94,26 +97,44 @@ def validate_entity(entity: dict):
       "_id": entity["_id"],
       "message": "Description is \"null\""
     })
+    # Set the description to an empty string
+    entity["description"] = ""
 
   if "attributes" in entity and len(entity["attributes"]) > 0:
     # Check that Attributes are valid
-    for attribute in entity["attributes"]:
-      validate_attribute(attribute)
+    validate_attributes(entity)
+  else:
+    entity["attributes"] = []
 
   if "history" in entity and len(entity["history"]) > 0:
     # Check that History is valid
     validate_history(entity)
+  else:
+    entity["history"] = []
 
 
-def validate_attribute(attribute):
-  # Check all required fields are present
-  missing_fields = list(set(ATTRIBUTE_FIELDS) - set(attribute.keys()))
-  if len(missing_fields) > 0:
-    logging.warning("Attribute \"{}\" missing {} fields: {}".format(attribute["_id"], len(missing_fields), missing_fields))
-    ISSUES.append({
-      "_id": attribute["_id"],
-      "message": "Missing {} fields: {}".format(attribute["_id"], len(missing_fields), missing_fields)
-    })
+def validate_attributes(entity):
+  for attribute in entity["attributes"]:
+    # Check all required fields are present
+    missing_fields = list(set(ATTRIBUTE_FIELDS) - set(attribute.keys()))
+    if len(missing_fields) > 0:
+      logging.warning("Attribute \"{}\" missing {} fields: {}".format(attribute["_id"], len(missing_fields), missing_fields))
+      ISSUES.append({
+        "_id": attribute["_id"],
+        "message": "Missing {} fields: {}".format(attribute["_id"], len(missing_fields), missing_fields)
+      })
+
+      # Add an owner if missing
+      if "owner" in missing_fields:
+        attribute["owner"] = entity["owner"]
+
+      # Add archive state if missing
+      if "archived" in missing_fields:
+        attribute["archived"] = False
+
+      # Add timestamp if missing
+      if "timestamp" in missing_fields:
+        attribute["timestamp"] = entity["timestamp"]
 
   # Check values
   for value in attribute["values"]:
@@ -137,8 +158,52 @@ def validate_history(entity):
         "message": "History missing {} fields: {}".format(len(missing_fields), missing_fields)
       })
 
+    if "_id" in missing_fields:
+      history["_id"] = entity["_id"]
+
+    if "version" in missing_fields:
+      history["version"] = nanoid.generate("1234567890abcdef", 10)
+
+    if "name" in missing_fields:
+      history["name"] = entity["name"]
+
+    if "created" in missing_fields:
+      history["created"] = entity["created"]
+
+    if "attachments" in missing_fields:
+      history["attachments"] = entity["attachments"]
+
+    if history["description"] is None:
+      logging.warning("Entity \"{}\" history description is \"null\"".format(entity["_id"]))
+      WARNINGS.append({
+        "_id": entity["_id"],
+        "message": "History description is \"null\""
+      })
+      history["description"] = ""
+
+    if "attributes" in history and len(history["attributes"]) > 0:
+        for attribute in history["attributes"]:
+          # Check all required fields are present
+          missing_history_attribute_fields = list(set(ATTRIBUTE_FIELDS) - set(attribute.keys()))
+          if len(missing_history_attribute_fields) > 0:
+            logging.warning("History attribute \"{}\" missing {} fields: {}".format(attribute["_id"], len(missing_history_attribute_fields), missing_history_attribute_fields))
+            ISSUES.append({
+              "_id": attribute["_id"],
+              "message": "Missing {} fields: {}".format(attribute["_id"], len(missing_history_attribute_fields), missing_history_attribute_fields)
+            })
+
+          if "timestamp" in missing_history_attribute_fields:
+            attribute["timestamp"] = history["timestamp"]
+
+          if "archived" in missing_history_attribute_fields:
+            attribute["archived"] = False
+
+          if "owner" in missing_history_attribute_fields:
+            attribute["owner"] = entity["owner"]
+
 
 def start():
+  collection = None
   with open(COLLECTION_PATH) as file:
     collection = json.load(file)
     logging.info("Collection: {} Entities".format(len(collection)))
@@ -146,9 +211,14 @@ def start():
     get_identifiers(collection)
     for e in collection:
       validate_entity(e)
+    logging.info("Finished: {} issues, {} warnings".format(len(ISSUES), len(WARNINGS)))
     file.close()
 
-  logging.info("Finished: {} issues, {} warnings".format(len(ISSUES), len(WARNINGS)))
+  if GENERATE_OUTPUT:
+    with open(OUTPUT_PATH, "w") as file:
+      json.dump(collection, file, ensure_ascii=False, indent=2)
+      logging.info("Wrote to path: {}".format(OUTPUT_PATH))
+      file.close()
 
 
 if __name__ == "__main__":
