8d381ca4815c06fcc8c3cdd4605f944b894969ea
Henry Burgess <henryjburg@gmail.com>
2024-10-03 22:42:55 -0500

    MARS-1056 Fix Jest configuration

--- client/jest.config.js
+++ client/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^@components/(.*)$": "<rootDir>/src/components/$1",
     "^@database/(.*)$": "<rootDir>/src/database/$1",
+    "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
     "^@pages/(.*)$": "<rootDir>/src/pages/$1",
     "^@types/(.*)$": "../types/$1",
     "src/variables": "<rootDir>/src/variables",
