63e598251fc8004179e13174294d874e9778104b
Henry Burgess <henryjburg@gmail.com>
2024-10-09 11:05:03 -0500

    MARS-1047 Begin restructuring tests

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
   },
 });
