84ab0941b1b347ccf81da3d8d305c6d8485982c6
Henry Burgess <henryjburg@gmail.com>
2024-10-14 07:40:25 -0500

    MARS-1047 Add `Project` Cypress tests

---
+++ client/test/cypress/tests/ui/project/project_edit_entities.spec.cy.ts
@@ -0,0 +1,71 @@
+describe("Project, edit Entities", () => {
+  it("should be able to add an Entity", () => {
+    cy.contains("button", "Projects").click();
+    cy.get("a").contains("View").eq(0).click();
+    cy.get("#editProjectButton").click();
+
+    // Add an Entity
+    cy.get("#addEntityButton").click();
+    cy.get("#entitySearchSelect").click();
+    cy.get("#entitySearchSelect").contains("button", "Test Entity").click();
+    cy.get("#addEntityDoneButton").click();
+    cy.get("#editProjectButton").click();
+
+    // Validate the Entity was added
+    cy.get("table").eq(0).contains("Test Entity");
+    cy.reload();
+    cy.get("table").eq(0).contains("Test Entity");
+  });
+
+  it("should be able to remove an Entity", () => {
+    cy.contains("button", "Projects").click();
+    cy.get("a").contains("View").eq(0).click();
+    cy.get("#editProjectButton").click();
+
+    // Add an Entity
+    cy.get("#addEntityButton").click();
+    cy.get("#entitySearchSelect").click();
+    cy.get("#entitySearchSelect").contains("button", "Test Entity").click();
+    cy.get("#addEntityDoneButton").click();
+    cy.get("#editProjectButton").click();
+    cy.reload();
+
+    // Remove the Entity
+    cy.get("#editProjectButton").click();
+    cy.get("tr > td > div > button").click();
+    cy.get("#editProjectButton").click();
+
+    // Validate the Entity was removed
+    cy.contains("table").should("not.exist");
+    cy.reload();
+    cy.contains("table").should("not.exist");
+  });
+
+  it("should be updated after removing the Project via the Entity", () => {
+    cy.contains("button", "Projects").click();
+    cy.get("a").contains("View").eq(0).click();
+    cy.get("#editProjectButton").click();
+
+    // Add an Entity
+    cy.get("#addEntityButton").click();
+    cy.get("#entitySearchSelect").click();
+    cy.get("#entitySearchSelect").contains("button", "Test Entity").click();
+    cy.get("#addEntityDoneButton").click();
+    cy.get("#editProjectButton").click();
+    cy.reload();
+
+    // Remove the the Entity
+    cy.get("tr > td > div > div > a").click();
+    cy.get("#editEntityButton").click();
+    cy.get("tr > td > div > button").eq(0).click();
+    cy.get("#editEntityButton").click();
+
+    // Navigate to the Project
+    cy.contains("button", "Projects").click();
+    cy.get("a").contains("View").eq(0).click();
+    cy.get("#editProjectButton").click();
+
+    // Validate the Entity was removed
+    cy.contains("table").should("not.exist");
+  });
+});
