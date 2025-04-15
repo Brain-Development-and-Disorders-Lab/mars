a4c684fef557ac54b3165b3cee3fd220ea786214
Henry Burgess <henryjburg@gmail.com>
2025-04-15 15:32:14 -0500

    MARS-1107 Create resolvers for `Counters`

--- server/src/models/Counters.ts
+++ server/src/models/Counters.ts
@@ -12,9 +12,7 @@ import { CounterModel, ICounter, ResponseData } from "@types";
 const COUNTERS_COLLECTION = "counters"; // Collection name
 
 export class Counters {
-  static createCounter = async (
-    counter: ICounter,
-  ): Promise<ResponseData<string>> => {
+  static create = async (counter: ICounter): Promise<ResponseData<string>> => {
     const joinedCounter: CounterModel = {
       _id: getIdentifier("counter"),
       ...counter,
@@ -45,6 +43,18 @@ export class Counters {
     return await getDatabase()
       .collection<CounterModel>(COUNTERS_COLLECTION)
       .findOne({ _id: _id });
+  };
+
+  /**
+   * Get all Counters associated with a Workspace
+   * @param workspace The current Workspace
+   * @return {Promise<CounterModel[]>}
+   */
+  static getCounters = async (workspace: string): Promise<CounterModel[]> => {
+    return await getDatabase()
+      .collection<CounterModel>(COUNTERS_COLLECTION)
+      .find({ workspace: workspace })
+      .toArray();
   };
 
   /**
@@ -88,17 +98,32 @@ export class Counters {
       };
     }
 
+    // Generate update object
+    const update: { $set: ICounter } = {
+      $set: {
+        workspace: counter.workspace,
+        name: counter.name,
+        current: counter.current,
+        increment: counter.increment,
+        format: counter.format,
+        created: counter.created,
+      },
+    };
+
     // Increment the Counter value and update
-    counter.current = counter.current + counter.increment;
+    update.$set.current = counter.current + counter.increment;
 
     // Update the stored Counter value
     const response = await getDatabase()
       .collection<CounterModel>(COUNTERS_COLLECTION)
-      .updateOne({ _id: _id }, counter);
+      .updateOne({ _id: _id }, update);
     const successStatus = response.modifiedCount === 1;
 
     // Generate the Counter value
-    const generated = counter.format.replace("{}", counter.current.toString());
+    const generated = counter.format.replace(
+      "{}",
+      update.$set.current.toString(),
+    );
 
     return {
       success: successStatus,
