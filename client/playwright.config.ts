c7bacd15006aa986bb8b5417f6994b852e32ad9d
Henry Burgess <henryjburg@gmail.com>
2025-12-16 17:01:09 -0600

    MARS-1135 Reuse server for Playwright

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   webServer: {
     command: "npm run start",
     url: "http://localhost:8080",
-    reuseExistingServer: process.env.CI ? false : true,
+    reuseExistingServer: true,
     timeout: 120 * 1000,
   },
 
