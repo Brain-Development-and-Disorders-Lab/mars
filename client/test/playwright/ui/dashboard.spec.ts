79739f22c899e2a61366d2e8c558442921b7dd3f
Henry Burgess <henryjburg@gmail.com>
2026-02-24 15:20:04 -0600

    MARS-1122 Fix `Dashboard` tests

---
+++ client/test/playwright/ui/dashboard.spec.ts
@@ -0,0 +1,20 @@
+import test, { expect } from "@playwright/test";
+import { performLogin } from "../helpers";
+
+test.describe("Interface launches", () => {
+  test.beforeEach(async ({ page }) => {
+    // Ensure the user is logged in
+    await performLogin(page);
+
+    // Navigate to the dashboard
+    await page.goto("/");
+  });
+
+  test("navigation menu items are visible", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.locator("#navSearchButtonDesktop")).toHaveText("Search");
+    await expect(page.locator("#navProjectsButtonDesktop")).toHaveText(
+      "Projects",
+    );
+  });
+});
