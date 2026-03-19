8bd42a08a273d0b21d224bdee3eee72264d6c020
Henry Burgess <henryjburg@gmail.com>
2026-03-19 16:12:36 -0500

    MARS-1122 Fix Webpack config for tests
    * `host` parameter explicitly set for `devServer`

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -6,6 +6,8 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
+    host: "127.0.0.1",
+    port: "8080",
     static: [
       {
         directory: path.join(__dirname, "dist"),
