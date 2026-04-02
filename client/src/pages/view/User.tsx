59188a85995400eebca2a0c784b92d3e782976b7
Henry Burgess <henryjburg@gmail.com>
2026-04-02 12:02:55 -0500

    MARS-1127 Update `Linky` appearance
    * Always show underline, but darken when hovered

--- client/src/pages/view/User.tsx
+++ client/src/pages/view/User.tsx
@@ -697,7 +697,7 @@ const User = () => {
                 <Text ml={"0.5"} textAlign={"left"} fontSize={"xs"} fontWeight={"semibold"}>
                   Avatar
                 </Text>
-                <ActorTag identifier={userModel._id} fallback={"Unknown User"} size={"md"} avatarOnly />
+                <ActorTag identifier={userModel._id.toString()} fallback={"Unknown User"} size={"md"} avatarOnly />
               </Flex>
 
               {/* Name */}
